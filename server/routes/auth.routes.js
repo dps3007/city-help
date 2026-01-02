b3503c77aceb595257cbb187249314b3f7ffc6c1
dps3007 <deepanshupal30072002@gmail.com>
2026-01-02 20:43:02 +0530

    rate-limiter

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -6,11 +6,12 @@ import {
   logout,
 } from '../controllers/auth.controller.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { authLimiter } from '../middlewares/rateLimit.middleware.js';
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register',authLimiter, register);
+router.post('/login', authLimiter,login);
 router.post('/refresh', refreshToken);
 router.post('/logout', verifyJWT, logout);
 
